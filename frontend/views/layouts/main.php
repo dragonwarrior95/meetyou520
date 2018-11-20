437cd295d537872f054f5b7d23c9536e712da7db
zwl <zwl@meitu.com>
2018-11-20 13:01:25 +0800

    微调

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -16,7 +16,7 @@ use yii\helpers\Html;
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="icon" href="img/fav-icon.png" type="image/x-icon" />
+    <link rel="icon" href="img/logo.png" type="image/x-icon" />
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
     <!-- Icon css link -->
